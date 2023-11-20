97b66a63874462bc515191119a052e5e73d8da99
QUYHUYNH-cmd <56834562+QUYHUYNH-cmd@users.noreply.github.com>
2023-11-20 08:04:06 +0700

    page.tsx

--- src/app/rewards/page.tsx
+++ src/app/rewards/page.tsx
@@ -8,6 +8,34 @@ export const metadata: Metadata = {
 
 export default function Rewards() {
   return (
-    <h1>Rewards</h1>
+    <div>
+      <div className='new-canvas_0'>
+        <div className='new-canvas_0-mask'></div>
+        <h1 className='new-canvas_0-text'>Power Market Rewards</h1>
+      </div>
+
+      <div className='new-canvas_1'>
+        <h2 className='new-canvas_1-text'>How It Works</h2>
+      </div>
+
+      <div className='new-canvas_2'></div>
+
+      <div className='new-canvas_3'></div>
+
+      <div className='new-canvas_4 d-flex justify-content-around mt-5'>
+        <div className='new-canvas_4-pic1'></div>
+        <div className='new-canvas_4-pic2'></div>
+      </div>
+
+      <div className='new-canvas_5 d-flex justify-content-around mt-5 mb-5'>
+        <div className='new-canvas_5-pic1 d-flex justify-content-center align-items-center flex-column'>
+          <h4 className='new-canvas_5-text'>Download Our App!</h4>
+          <div className='d-flex justify-content-around w-100'>
+            <div className='new-canvas_5-pic2'></div>
+            <div className='new-canvas_5-pic3'></div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
