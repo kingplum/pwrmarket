f3611dc32abb5fe027843aece6cc0d99e36a1555
QUYHUYNH-cmd <56834562+QUYHUYNH-cmd@users.noreply.github.com>
2023-11-22 16:31:02 +0700

    Add files via upload

---
+++ src/app/locations/Data.js
@@ -0,0 +1,137 @@
+const CategoryListData = {
+    'type': 'FeatureCollection',
+    'features': [
+      {
+        'type': 'Feature',
+        'lat': 16.003044,
+        'lng': 108.254687,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      },
+      {
+        'type': 'Feature',
+        'lat': 15.962728,
+        'lng': 107.853492,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 507-8357',
+          'phone': '2025078357',
+          'address': '2221 I St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 22nd St NW',
+          'postalCode': '20037',
+          'state': 'D.C.',
+          'storeId': '9875'
+        }
+      },{
+        'type': 'Feature',
+        'lat': 15.904877,
+        'lng': 108.313013,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      },{
+        'type': 'Feature',
+        'lat': 15.805541,
+        'lng': 15.805541,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      },{
+        'type': 'Feature',
+        'lat': 16.728566,
+        'lng': 107.094904,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      },{
+        'type': 'Feature',
+        'lat': 14.917132,
+        'lng': 108.103622,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      },{
+        'type': 'Feature',
+        'lat': 15.283186,
+        'lng': 107.155758,
+        'geometry': {
+          'type': 'Point'
+        },
+        'properties': {
+          'phoneFormatted': '(202) 234-7336',
+          'phone': '2022347336',
+          'address': '1471 P St NW',
+          'city': 'Washington DC',
+          'country': 'United States',
+          'crossStreet': 'at 15th St NW',
+          'postalCode': '20005',
+          'state': 'D.C.',
+          'storeId': '9874'
+        }
+      }
+    ]
+  }
+
+export default{
+    CategoryListData
+}
